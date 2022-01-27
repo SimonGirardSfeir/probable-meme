f42fc9ca6a87d727262f0c0b9057d423da2b94e0
Simon Girard <simon.girard1991@gmail.comm>
2022-01-27 19:26:20 +0100

    day1 refactoring

--- src/main/java/com/girardsimon/adventofcode2021/problems/Day1Resolver.java
+++ src/main/java/com/girardsimon/adventofcode2021/problems/Day1Resolver.java
@@ -2,42 +2,40 @@ package com.girardsimon.adventofcode2021.problems;
 
 import java.util.List;
 
-public class Day1Resolver implements Resolver {
+public class Day1Resolver {
 
-    @Override
-    public int part1(List<String> lines) {
-        int output = 0;
-        int[] intsFromFiles = mapLinesToIntArray(lines);
+    List<String> depths;
 
-        for(int i = 0; i < intsFromFiles.length - 1; i++) {
-            if (intsFromFiles[i + 1] > intsFromFiles[i]) {
-                output++;
-            }
-        }
-        return output;
+    public Day1Resolver(List<String> depths) {
+        this.depths = depths;
     }
 
-    @Override
-    public int part2(List<String> lines) {
-        int output = 0;
-
-        int[] intsFromFiles = mapLinesToIntArray(lines);
-
-        for(int i = 0; i < intsFromFiles.length - 3; i++) {
-            int firstSumThreeConsecutives = intsFromFiles[i] + intsFromFiles[i+1]
-                    + intsFromFiles[i+2];
-            int secondSumThreeConsecutives = intsFromFiles[i+1] + intsFromFiles[i+2]
-                    + intsFromFiles[i+3];
-            if (secondSumThreeConsecutives > firstSumThreeConsecutives) {
-                output++;
-            }
-        }
-
-        return output;
-    }
-
-    private int[] mapLinesToIntArray(List<String> lines) {
-        return lines.stream()
+    public int[] mapLinesToIntArray() {
+        return depths.stream()
                 .map(Integer::parseInt).mapToInt(i -> i).toArray();
+    }
+
+    public int countNumberOfDepthIncreasing(int windowMeasurementSize) {
+        var arrayOfDepths = mapLinesToIntArray();
+        int numberOfDepthIncreasing = 0;
+        for (int i = 0; i < arrayOfDepths.length- windowMeasurementSize; i++) {
+            int firstSum = getSumForWindow(i, windowMeasurementSize);
+            int secondSum = getSumForWindow(i+1, windowMeasurementSize);
+
+            if(secondSum > firstSum) {
+                numberOfDepthIncreasing++;
+            }
+        }
+        return numberOfDepthIncreasing;
+    }
+
+    public int getSumForWindow(int currentIndex, int windowMeasurementSize) {
+        var arrayOfDepths = mapLinesToIntArray();
+        int sumForWindows = arrayOfDepths[currentIndex];
+        for(int i = 1; i < windowMeasurementSize; i++) {
+            sumForWindows += arrayOfDepths[currentIndex + i];
+        }
+
+        return sumForWindows;
     }
 }
--- src/test/java/com/girardsimon/adventofcode2021/problems/Day1ResolverTest.java
+++ src/test/java/com/girardsimon/adventofcode2021/problems/Day1ResolverTest.java
@@ -3,27 +3,95 @@ package com.girardsimon.adventofcode2021.problems;
 import com.girardsimon.adventofcode2021.utils.UtilsClass;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class Day1ResolverTest {
 
     List<String> linesFromFile;
-    Resolver resolver;
+    Day1Resolver resolver;
+    List<String> simpleDataSampleForDepths;
+    Day1Resolver resolverWithBasicData;
 
     @BeforeEach
     void setUp() throws IOException {
         linesFromFile = UtilsClass.getLines("src/test/resources/day1/data.txt");
-        resolver = new Day1Resolver();
+        resolver = new Day1Resolver(linesFromFile);
+        simpleDataSampleForDepths = Arrays.asList("199", "200", "208", "210", "200", "207", "240", "269", "260", "263");
+        resolverWithBasicData = new Day1Resolver(simpleDataSampleForDepths);
     }
 
     @Test
+    void mapLinesToIntArray_should_map_List_of_String_to_a_List_of_Integer_if_data_corresponding_to_number() {
+        //Given
+        var givenList = Arrays.asList("123", "456", "789");
+        var givenDay1Resolver = new Day1Resolver(givenList);
+
+        //When
+        var actualList = givenDay1Resolver.mapLinesToIntArray();
+
+        //Then
+        var expectedList = new int[] {123, 456, 789};
+        assertThat(actualList).containsExactly(expectedList);
+    }
+
+    @Test
+    void mapLinesToIntArray_should_throw_NumberFormatException_if_data_contains_at_least_one_element_which_did_not_match_a_number() {
+        //Given
+        var givenList = Arrays.asList("123", "Coucou", "789");
+        var givenDay1Resolver = new Day1Resolver(givenList);
+
+        //When...Then
+        assertThrows(NumberFormatException.class, givenDay1Resolver::mapLinesToIntArray);
+    }
+
+    @Test
+    void getSumForWindow_should_return_sum_asociated_with_current_index_and_window_size() {
+        //Given
+        int currentIndex = 0;
+        int windowMeasurementSize = 3;
+
+        //When
+        int actualSumForWindow = resolverWithBasicData.getSumForWindow(currentIndex, windowMeasurementSize);
+
+        //Then
+        int expectedSumForWindow = 607;
+        assertThat(actualSumForWindow).isEqualTo(expectedSumForWindow);
+    }
+
+    private static Stream<Arguments> inputsForcountNumberOfDepthIncreasing() {
+        return Stream.of(
+                Arguments.of(1, 7),
+                Arguments.of(3, 5)
+        );
+    }
+
+    @MethodSource("inputsForcountNumberOfDepthIncreasing")
+    @ParameterizedTest
+    void countNumberOfDepthIncreasing_should_compute_number_of_depth_increasing_according_to_given_sliding_window(int windowMeasurementSize, int expectedNumberOfDepthIncresing) {
+        //When
+        int actualNumberOfDepthIncreasing = resolverWithBasicData.countNumberOfDepthIncreasing(windowMeasurementSize);
+
+        assertThat(actualNumberOfDepthIncreasing).isEqualTo(expectedNumberOfDepthIncresing);
+    }
+
+
+    /**
+     * I keep these unit tests only to conserve my aoc result!
+     */
+    @Test
     void part1_should_calculate_number_of_times_depth_measurement_increases() {
         //When
-        int actualNumberOfTimes = resolver.part1(linesFromFile);
+        int actualNumberOfTimes = resolver.countNumberOfDepthIncreasing(1);
 
         //Then
         int expectedNumberOfTimes = 1288;
@@ -33,7 +101,7 @@ class Day1ResolverTest {
     @Test
     void part2_should_calculate_number_of_times_sums_of_three_consecutives_depth_measurement_increases() {
         //When
-        int actualNumberOfTimes = resolver.part2(linesFromFile);
+        int actualNumberOfTimes = resolver.countNumberOfDepthIncreasing(3);
 
         //Then
         int expectedNumberOfTimes = 1311;
